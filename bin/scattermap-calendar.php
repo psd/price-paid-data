c93d6c350c42b9c66e8e1fce380cc670179438b2
psd <psd@whatfettle.com>
2014-11-14 19:17:38 +0000

    Calendar of plots

--- bin/scattermap-calendar.php
+++ bin/scattermap-calendar.php
@@ -1,5 +1,17 @@
 #!/usr/bin/env php
-<!DOCTYPE html>
+<?php
+    $imgdir = "../out/mapination";
+
+    # return ISO date string for a julian date
+    function jdtodate($jd) {
+        list($month, $day, $year) = explode('/', jdtogregorian($jd));
+        return sprintf("%4d-%02d-%02d", $year, $month, $day);
+    }
+
+    $ywidth = '42.5mm';
+    $width = '8.5mm';
+
+?><!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -10,103 +22,117 @@
     border: none;
 }
 body {
-    font-family: "Helvetica Neue", sans-serif;
+    font-family: "Suisse Int'l Bold", "Helvetica Neue", sans-serif;
 }
 .page {
     width: 915mm;
     height: 630mm;
     overflow: hidden;
+    border-bottom: 1px solid red;
 }
-td {
-  vertical-align: top;
+.year {
+    width: <?= $ywidth ?>;
+    float: left;
+    border-right: 3mm solid white;
+    overflow: hidden;
+    margin-top: 10mm;
 }
-.calendar {
-    margin-top: 5mm;
+.letters {
+    width: <?= $ywidth; ?>;
+    overflow: hidden;
+    height: 7.5mm;
 }
-caption, th {
+h2, h3 {
+    display: block;
+    float: left;
     font-weight: bold;
+    text-align: center;
     color: #eee;
 }
-.year td {
-    position: relative;
-    overflow: hidden;
-    width: 10mm;
-    height: 10mm;
+h2 {
+    width: <?= $ywidth ?>;
 }
-.S {
-    display: none;
+h3 {
+    width: <?= $width ?>;
 }
-.year td img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
+.blank {
+    min-width: <?= $width ?>;
+}
+.day {
+  display: block;
+  float: left;
+  position: relative;
+  overflow: hidden;
+  height: <?= $width ?>;
+  max-width: <?= $width ?>;
+}
+.day .spacer {
+  width: <?= $width ?>;
+  height: <?= $width ?>;
+}
+.day .sprite {
+  position: absolute;
+  top: 0;
+  left: 0;
+  max-width: none;
+  max-height: 100%;
 }
 </style>
 </head>
 <body>
 <div class="page">
 <h1>Volume of Land Registry Transactions</h1>
-<table class="calendar">
-<tr>
+<div class="calendar">
 <?php
 date_default_timezone_set("GMT");
-
-$imgdir = "../out/mapination/";
 
 # one column per-year
 for ($year=1995; $year <= gmdate("Y"); $year++) {
 
     # year titles
-    echo "<td>\n";
-    echo "<table class='year-$year year'>\n";
-    echo "<caption>$year</caption>\n";
+    echo "<div class='year year-$year'>\n";
+    echo "<h2>$year</h2>\n";
 
     # day coulmn titles
     $letters = array('S','M','T','W','T','F','S');
-    echo "<tr>\n";
+    echo "<div class='letters'>\n";
     foreach($letters as $letter) {
-        echo "    <th class='header $letter'>$letter</th>\n";
+        if ($letter != 'S') {
+            echo "  <h3>$letter</h3>\n";
+        }
     }
-    echo "</tr>\n";
+    echo "</div>\n";
 
     # days in first week from previous year
     $firstday = jddayofweek(gregoriantojd(1, 1, $year));
     if ($firstday > 0) {
-        echo "<tr>\n";
         for ($weekday = 0; $weekday < jddayofweek(gregoriantojd(1, 1, $year)); $weekday++) {
             $letter = $letters[$weekday];
-            echo "    <td class='blank $letter'>&nbsp;</td>\n"; 
+            if ($letter != 'S') {
+                echo "  <div class='day blank $letter'>&nbsp;</div>\n"; 
+            }
         }
     }
 
     # days of the year
     for ($jd = gregoriantojd(1, 1, $year); $jd <= gregoriantojd(12, 31, $year); $jd++) {
 
-        $month = jdmonthname($jd, 0);
+        $date = jdtodate($jd);
         $weekday = jddayofweek($jd, 0);
-        list($monthno, $monthday, $y) = explode('/', jdtogregorian($jd));
-
-        $date = sprintf("%4d-%02d-%02d", $year, $monthno, $monthday);
         $letter = $letters[$weekday];
+        $mdate = substr($date, 0, 7);
+        $left = (intval(substr($date, -2)) -1) * 100;
 
-        if ($weekday == 0) {
-            echo "<tr>\n";
-        }
-        echo "    <td id='$date' class='day $letter jd_$jd $month'>";
-        #printf("<img src='%ssprites-%04d-%02d.gif' style='left:-%d%s'>", $imgdir, $year, $monthno, ($monthday-1)/32*100, '%');
-        echo "<img src='$imgdir$date.gif' title='$date'>";
-        echo "</td>\n";
-        if ($weekday == 6) {
-            echo "</tr>\n";
+        if ($letter != 'S') {
+            echo "<div class='day'>";
+            echo "<img class='spacer' src='$imgdir/1994-01-01.gif'>";
+            echo "<img class='sprite' src='$imgdir/sprites-$mdate.gif' style='left:-$left%' title='$date'>";
+            echo "</div>\n";
         }
     }
-    echo "</tr>\n</table>\n</td>\n";
+    echo "</div>\n";
 }
 ?>
-</tr></table>
 </div>
 </body>
-</script>
 </html>
