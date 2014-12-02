9f12d7335426f19992fb5cfd112e5684ad7861ab
psd <psd@whatfettle.com>
2014-12-02 07:36:39 +0000

    Fixed broken gnuplot script

--- bin/pricesmooth.R
+++ bin/pricesmooth.R
@@ -1,9 +1,5 @@
 #!/usr/bin/env r --slave -f
 
-library(ggplot2)
-
 prices <- read.csv("data/prices.tsv", sep="	")
-#png("out/pricesmooth.png", width=640, height=480)
-#plot(prices[,1], prices[2], ylim(0, 600000), col="#c8c8c8", span=0.5,)
-
-ggplot(prices, aes(p[,1], p[,2])
+png("out/pricesmooth.png", width=640, height=480)
+plot(prices[,1])
